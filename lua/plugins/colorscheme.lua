701036a277c99688911679f343f457bd9859419d
Ronit Paul <ronitpaulindia@gmail.com>
2025-11-05 20:00:04 +0530

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,33 @@
-return { 
-  "folke/tokyonight.nvim", 
-  lazy = false, 
-  priority = 1000, 
-  config = function() 
-    require("tokyonight").setup({ 
-      style = "night", 
-      transparent = true, 
-      styles = { 
-        sidebars = "transparent", 
-        floats = "transparent", 
-      }, 
-    }) 
-    vim.cmd([[colorscheme tokyonight]]) 
-  end, 
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha", -- options: latte, frappe, macchiato, mocha
+      transparent_background = true,
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        functions = { "bold" },
+        keywords = { "bold" },
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      integrations = {
+        telescope = true,
+        nvimtree = true,
+        lsp_trouble = true,
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+      },
+    })
+    vim.cmd([[colorscheme catppuccin]])
+  end,
 }
-
