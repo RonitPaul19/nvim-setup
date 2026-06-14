4f17e124fa8cfdbcc3105684ca243f3a6a15695e
RonitPaul19 <ronitpaulindia@gmail.com>
2026-06-14 15:13:55 +0530

    added lsp for ts_ls i.e. react, next, vue etc

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -9,30 +9,69 @@ vim.lsp.config("*", {
   capabilities = capabilities,
 })
 
-vim.lsp.config('clangd', {
-  cmd = { 'clangd' },
-  filetypes = { 'c', 'cpp' },
-  root_markers = { 'compile_commands.json', '.clangd', '.git' },
+vim.lsp.config("clangd", {
+  cmd = { "clangd" },
+  filetypes = { "c", "cpp" },
+  root_markers = { "compile_commands.json", ".clangd", ".git" },
 })
 
-vim.lsp.config('pyright', {
-  cmd = { 'pyright-langserver', '--stdio' },
-  filetypes = { 'python' },
-  root_markers = { 'pyproject.toml', 'setup.py', '.git' },
+vim.lsp.config("pyright", {
+  cmd = { "pyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_markers = { "pyproject.toml", "setup.py", ".git" },
+})
+
+vim.lsp.config("ts_ls", {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+  },
+  root_markers = {
+    "package.json",
+    "tsconfig.json",
+    "jsconfig.json",
+    ".git",
+  },
 })
 
 vim.lsp.enable({
-  'clangd',
-  'pyright',
-  'ts_ls',
-  'html',
-  'cssls',
-  'tailwindcss',
-  'jsonls',
-  'emmet_language_server',
-  'vue_ls',
-  'bashls',
-  'lua_ls',
+  "clangd",
+  "pyright",
+  "ts_ls",
+  "html",
+  "cssls",
+  "tailwindcss",
+  "jsonls",
+  "emmet_language_server",
+  "vue_ls",
+  "bashls",
+  "lua_ls",
 })
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(event)
+    local opts = { buffer = event.buf }
 
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+    vim.keymap.set("n", "<leader>f", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+  end,
+})
